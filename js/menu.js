e641ee8977194f930c0d0f0a16da25b3bad27361
Oleksandr <71338915+superbowl-alex@users.noreply.github.com>
2022-05-07 16:05:03 +0300

    Fixed menu

--- js/menu.js
+++ js/menu.js
@@ -5,6 +5,7 @@
     const expanded =
       menuBtnRef.getAttribute("aria-expanded") === "true" || false;
     menuBtnRef.classList.toggle("is-open");
+    document.body.classList.toggle("menu-open");
     menuBtnRef.setAttribute("aria-expanded", !expanded);
     mobileMenuRef.classList.toggle("is-open");
   });
